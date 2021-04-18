62a528e24fb37ab5241ac76108a678577da4ceba
yunhsuanchin <alicechin20181@gmail.com>
2021-04-18 16:02:32 +0800

    feat: add user model

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const db = require('./models')
 const app = express()
 const PORT = 3000
 
 app.listen(PORT, () => {
+  db.sequelize.sync()
   console.log(`App is listening on port ${PORT}`)
 })
