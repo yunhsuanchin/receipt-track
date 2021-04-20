1af46ceb0de58fe50b9b9c6783e75fe4b39abf97
yunhsuanchin <alicechin20181@gmail.com>
2021-04-20 15:35:49 +0800

    fix: modify dataTypes

--- migrations/20210418080842-create-receipt-product.js
+++ migrations/20210418080842-create-receipt-product.js
@@ -22,13 +22,11 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+        type: Sequelize.DATE
       }
     });
   },
