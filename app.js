751064b4724b92d1a86bbaeadcc12a4ad944cc3c
yunhsuanchin <alicechin20181@gmail.com>
2021-04-20 15:37:59 +0800

    feat: add procfile and modify port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const db = require('./models')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
